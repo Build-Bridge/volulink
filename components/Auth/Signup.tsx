cf69f284d469270be5ca73a5e6177ddd3860d4f5
Black Bobs <ayodejiayodele350@gmail.com>
2024-10-11 13:44:49 +0100

    ...

--- components/Auth/Signup.tsx
+++ components/Auth/Signup.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { IoMdArrowBack } from "react-icons/io";
+import ControlledInput from '../Reusables/ControlledInput';
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='w-full max-w-[430px] mx-auto py-10'>
+        <div>
+            <IoMdArrowBack size={25} />
+        </div>
+        <div className='my-5'>
+            <h1 className='font-medium text-[32px] leading-[40px] my-2'>Let&apos;s get started</h1>
+            <p className='text-[#00030AA3] text-[14px] leading-[21px]'>Are you a volunteer looking for oppurtunities? you are just one click away.</p>
+
+
+            <form className='my-5 space-y-5'>
+                <div>
+                    <ControlledInput type="email" placeholder='Enter your email address' />
+                </div>
+                <div>
+                    <ControlledInput type="password" placeholder='Password' />
+                </div>
+                <div>
+                    <ControlledInput type="password" placeholder='Confirm Password' />
+                </div>
+                <div></div>
+            </form>
+        </div>
+    </div>
   )
 }
 
--- components/Reusables/AuthButton.tsx
+++ components/Reusables/AuthButton.tsx
@@ -1,12 +1,23 @@
 import Image from 'next/image'
 import React from 'react'
 
-const AuthButton = () => {
+interface AuthButtonProp {
+  icon: string;
+  text: string;
+  textColor: string;
+  bgColor: string;
+}
+
+const AuthButton: React.FC<AuthButtonProp> = ({icon, text, textColor, bgColor}) => {
   return (
     <div className='w-full max-w-[400px] flex items-center justify-center m-2'>
-   <button className='w-full text-center bg-transparent text-black font-semibold leading-4 p-3 rounded-md flex items-center justify-center gap-3 border border-[#00030A52]'>
-    {/* <Image src={'/apple.svg'} alt='google' priority width={20} height={20} /> */}
-   Sign up
+   <button className={`w-full text-center bg-transparent text-black font-semibold leading-4 p-3 rounded-md flex items-center justify-center gap-3 border border-[#00030A52]`}>
+    {
+      icon && (
+      <Image src={icon} alt={text} priority width={20} height={20} /> 
+      )
+    }
+   {text}
    </button>
     </div>
   )
