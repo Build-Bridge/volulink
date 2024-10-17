003419948f483c70667aea5148771912690c870d
Black Bobs <ayodejiayodele350@gmail.com>
2024-10-17 17:34:53 +0100

    ...

---
+++ app/auth/signup/confirm/page.tsx
@@ -0,0 +1,12 @@
+import ConfirmSignup from '@/components/Auth/ConfirmSignup'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <ConfirmSignup />
+    </div>
+  )
+}
+
+export default page
