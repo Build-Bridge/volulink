f94ae96855d3dafeb22613f16d4fdb4c221191b2
oyetunji26 <oyetunjie5@gmail.com>
2024-10-23 14:19:37 +0100

    Added Home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
+import TopNav from "@/components/Navbar/TopNav";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={` antialiased`}
+      >
+        <TopNav />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
