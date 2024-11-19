c2b79a3c25490c77bcbf76e817b9cac9f0c473ba
Black Bobs <ayodejiayodele350@gmail.com>
2024-11-19 02:05:35 +0100

    feat:job-modal

--- components/Modals/SearchModal.tsx
+++ components/Modals/SearchModal.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const SearchModal = () => {
   const searchList = [""];
   return (
-    <div className="bg-white w-[400px] h-fit overflow-visible">
+    <div className="bg-white w-[345px] min-h-[200px] border border-[#000E2B] shadow-lg shadow-[#10182814] text-[#00030A] rounded-lg">
       <ul>
         {Array.from({ length: 6 }).map((_, index) => (
-          <li key={index}>UI/Ux Designer</li>
+          <li key={index} className="p-2 font-medium leading-6">UI/Ux Designer</li>
         ))}
       </ul>
     </div>
